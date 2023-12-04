013afd7908b675d664d65f6bb531d9d0501af4db
Lydia <lydiadoula@gmail.com>
2023-12-04 10:44:38 +0000

    deploy please

--- src/components/searchbar/page.tsx
+++ src/components/searchbar/page.tsx
@@ -4,15 +4,15 @@ import React, { useState } from 'react';
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
-  const handleSearch = (event) => {
+  const handleSearch = (event:React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
 
     // Add logic to filter and display content based on searchTerm
     const contentElements = document.querySelectorAll('.searchable-content');
     contentElements.forEach((element) => {
-      const contentText = element.textContent.toLowerCase();
+      const contentText = (element.textContent || '').toLowerCase();
       const isVisible = contentText.includes(searchTerm.toLowerCase());
-      element.style.display = isVisible ? 'block' : 'none';
+      (element as HTMLElement).style.display = isVisible ? 'block' : 'none';
     });
   };
 
