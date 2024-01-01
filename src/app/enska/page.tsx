11aa1b0982c90a9ec843a6277aedd7df8848a75e
Lydia <lydiadoula@gmail.com>
2024-01-01 12:56:05 +0000

    some fixes

--- src/app/enska/page.tsx
+++ src/app/enska/page.tsx
@@ -24,9 +24,20 @@ function English() {
     const [fetchError, setFetchError] = useState("")
     const [english, setEnglish] = useState(null) as [Data[] | null, (english: Data[] | null) => void]
 
-    const get=(name:string) => {
-        return english?.filter(content => content.name === name) [0].value as string
-    }
+    const get = (name: string) => {
+        // Check if frontpage is not null and not empty
+        if (english && english.length > 0) {
+          const filteredContent = english.filter(item => item.name === name);
+          // Check if any matching content was found
+          if (filteredContent.length > 0) {
+            return filteredContent[0].value as string;
+          } else {
+            return ""; // Return an empty string if no matching content found
+          }
+        } else {
+          return ""; // Return an empty string if frontpage is empty or null
+        }
+      };
 
     useEffect(() => {
         const fetchEnglish = async () => {
@@ -55,36 +66,64 @@ function English() {
 
     return(
         <div>
-            <Navbar/>
-            <h1 className='welcome-h1'>Welcome to<br/> <span>Læknasetrið</span></h1>
-            <img className='main-img' src='/main.jpg' alt='stethoscope' />
+        <Navbar/>
+        <h1 className='welcome-h1'>Welcome to<br/> <span>Læknasetrið</span></h1>
+        <img className='main-img' src='/main8.avif' alt='stethoscope' />
 
-            <div className='info-box'>
-                <div className='iconbtn'>
-                    <img className='icon' src='/icondoctors.svg' alt='doctor icon' />
-                    <Link href='/laeknar'>
-                        <div>
-                            <div className='info-btn2'>Doctors</div>
-                        </div>
-                    </Link>
-                </div>
-                <div className='phone'>
-                    <img className='icon' src='/iconphone.svg' alt='phone'/>
+        <div className='phoneclockcontainer'>
+            <div className='iconbtn'>
+                <div className='icon-box'>
+                    <img className='icon' src='/phoneicon.png' alt='phone'/>
+                    <p>Phone number</p>
                     {error && <p>{error}</p>}
-                        <div>
-                            <h2 className='phonenumber'>{get('Phone number')}</h2>
-                        </div>
-
+                    <div>
+                        <p className='info-btn2'>{get('Phone number')}</p>
+                    </div>
                 </div>
-                <div className='iconbtn'>
-                    <img className='icon' src='/iconheart2.svg' alt='doctor icon' />
+            </div>
+
+            <div className='iconbtn'>
+            {error && <p>{error}</p>}
+                <Link href='/laeknar'>
+                    <div className='icon-box'>
+                        <img className='icon' src='/clockicon.png' alt='opening hours icon' />
+                        <p className='info-btn2'>Opening Hours</p>
+                        <p className='oh'>{get('oh Monday')}</p>
+                        <p className='oh'>{get('oh Friday')}</p>
+                    </div>
+                </Link>
+            </div>
+        </div>
+
+    <h2>Welcome to Læknasetrið</h2>
+    <p className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+        officia deserunt mollit anim id est laborum.</p>
+    <div className='midsectioncontainer'>
+        <div className='midsectionbtn'>
+            <img className='icon2' src='/doctorsicon.png' alt='doctor icon' />
+            <div className='textandbtn'>
+                <p className='info-btn2'>Doctors</p>
+                <p>See all the doctors and other staff of Læknasetrið</p>
+                <Link href='/laeknar'>
+                    <button>Doctors</button>
+                </Link>
+            </div>
+        </div>
+
+            <div className='midsectionbtn'>
+                <img className='icon2' src='/ecgicon.png' alt='doctor icon' />
+                <div className='textandbtn'>
+                    <p className='info-btn2'>Tests</p>
+                    <p>Read about all the tests performed in Læknasetrið</p>
                     <Link href='/rannsoknir'>
-                        <div>
-                            <div className='info-btn2'>Tests</div>
-                        </div>
+                        <button>Tests</button>
                     </Link>
                 </div>
             </div>
+        </div>
 
 
         {error && <p>{error}</p>}
