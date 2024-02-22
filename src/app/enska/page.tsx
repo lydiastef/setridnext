b41c85001279ce2ba6ceef91c117c7a1a0f05c89
Lydia <lydiadoula@gmail.com>
2024-02-22 12:13:50 +0000

    english

--- src/app/enska/page.tsx
+++ src/app/enska/page.tsx
@@ -6,9 +6,6 @@ import supabase from '../../config/supabaseClient';
 import { useEffect, useState } from 'react'
 import Link from 'next/link';
 
-
-/*import { useRouter } from 'next/navigation';*/
-
 type Data = {
     created_at: string;
     id: number;
@@ -16,18 +13,18 @@ type Data = {
     value: string | null;
 }
 
-function English() {
+function Frontpage() {
 
     const [content, setContent] = useState({});
     const [error, setError] = useState(null);
 
     const [fetchError, setFetchError] = useState("")
-    const [english, setEnglish] = useState(null) as [Data[] | null, (english: Data[] | null) => void]
+    const [frontpage, setFrontpage] = useState(null) as [Data[] | null, (frontpage: Data[] | null) => void]
 
     const get = (name: string) => {
         // Check if frontpage is not null and not empty
-        if (english && english.length > 0) {
-          const filteredContent = english.filter(item => item.name === name);
+        if (frontpage && frontpage.length > 0) {
+          const filteredContent = frontpage.filter(item => item.name === name);
           // Check if any matching content was found
           if (filteredContent.length > 0) {
             return filteredContent[0].value as string;
@@ -40,99 +37,104 @@ function English() {
       };
 
     useEffect(() => {
-        const fetchEnglish = async () => {
+        const fetchFrontpage = async () => {
             const { data, error } = await supabase
-            .from('english') //fetching data from this table in Supabase
+            .from('frontpage') //fetching data from this table in Supabase
             .select()
 
             if(error) {
                 setFetchError('Could not fetch Where')
-                setEnglish(null)
+                setFrontpage(null)
                 console.log(error)
             }
             if (data) {
-                setEnglish(data)
+                setFrontpage(data)
                 setFetchError("")
             }
         }
 
-        fetchEnglish();
+        fetchFrontpage();
     }, [])
 
     //Fetching data from Supabase - end
 
-    console.log(english)
+    console.log(frontpage)
     if(fetchError) return <p>{fetchError}</p>
 
     return(
         <div>
-        <Navbar/>
-        <h1 className='welcome-h1'>Welcome to<br/> <span>Læknasetrið</span></h1>
-        <img className='main-img' src='/main8.avif' alt='stethoscope' />
+            <Navbar/>
+            <div className='imgandtext'>
+                <img className='main-img' src='/setrid2.jpg' alt='stethoscope' />
+            </div>
 
-        <div className='phoneclockcontainer'>
-            <div className='iconbtn'>
-                <div className='icon-box'>
-                    <img className='icon' src='/phoneicon.png' alt='phone'/>
-                    <p>Phone number</p>
-                    {error && <p>{error}</p>}
+            <div className='phoneclockcontainer'>
+
+            <div className='iconbtn1'> 
+                <img className='icon2' src='/emailicon.png' alt='email icon' />
+                <h2 className='undericon'>Email</h2>
+                {error && <p>{error}</p>}
                     <div>
-                        <p className='info-btn2'>{get('Phone number')}</p>
+                        <p className='info-btn2'>{get('email')}</p>
                     </div>
-                </div>
             </div>
 
             <div className='iconbtn'>
-            {error && <p>{error}</p>}
-                <Link href='/laeknar'>
-                    <div className='icon-box'>
-                        <img className='icon' src='/clockicon.png' alt='opening hours icon' />
-                        <p className='info-btn2'>Opening Hours</p>
-                        <p className='oh'>{get('oh Monday')}</p>
-                        <p className='oh'>{get('oh Friday')}</p>
-                    </div>
-                </Link>
-            </div>
-        </div>
+                    <img className='icon2' src='/clockicon.png' alt='clock icon' />
+                    <h2 className='undericon'>Opening Hours</h2>
+                    {error && <p>{error}</p>}
+                    <p className='info-btn2'>{get('oh Monday')}</p>
+                    <p className='info-btn2'>{get('oh Friday')}</p>
+                </div>
 
-    <h2>Welcome to Læknasetrið</h2>
-    <p className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
-        officia deserunt mollit anim id est laborum.</p>
-    <div className='midsectioncontainer'>
-        <div className='midsectionbtn'>
-            <img className='icon2' src='/doctorsicon.png' alt='doctor icon' />
-            <div className='textandbtn'>
-                <p className='info-btn2'>Doctors</p>
-                <p>See all the doctors and other staff of Læknasetrið</p>
-                <Link href='/laeknar'>
-                    <button>Doctors</button>
-                </Link>
-            </div>
-        </div>
-
-            <div className='midsectionbtn'>
-                <img className='icon2' src='/ecgicon.png' alt='doctor icon' />
-                <div className='textandbtn'>
-                    <p className='info-btn2'>Tests</p>
-                    <p>Read about all the tests performed in Læknasetrið</p>
-                    <Link href='/rannsoknir'>
-                        <button>Tests</button>
-                    </Link>
+                <div className='iconbtn1'>
+                    <img className='icon2' src='/phoneicon.png' alt='phone'/>
+                    <h2 className='undericon'>Phone number</h2>
+                        {error && <p>{error}</p>}
+                        <div>
+                            <p className='info-btn2'>{get('Phone number')}</p>
+                        </div>
                 </div>
             </div>
-        </div>
+            
+        <div className='main-container'>
+            <h2>Welcome to Læknasetrið</h2>
+            <p className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+            aliquip ex ea commodo consequat.<br></br><br></br> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+            officia deserunt mollit anim id est laborum.</p>
+        
+        
+            <div className='midsectioncontainer'>
+                <div className='midsectionbtn'>
+                    <div className='textandbtn'>
+                    <img className='icon' src='/doctorsicon.png' alt='doctor icon' />
+                        <p className='box-p'>See all the doctor who work in Læknasetrið</p>
+                        <Link href='/laeknar'>
+                            <button>Doctors</button>
+                        </Link>
+                    </div>
+                </div>
 
+                <div className='midsectionbtn'>
+                    <div className='textandbtn'>
+                    <img className='icon3' src='/ecgicon.png' alt='doctor icon' />
+                        <p className='box-p'>Read about all the tests and services offered at Læknasetrið</p>
+                        <Link href='/frontpageadmin'>
+                            <button>Tests</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
 
-        {error && <p>{error}</p>}
+            {error && <p>{error}</p>}
  
             <h2 className='where-h2'>{get('title')}</h2>
             
-        <div className='where'>
-            <div className='where1'>
-                {error && <p>{error}</p>}
+            <div className='where'>
+                <div className='where1'>
+                    {error && <p>{error}</p>}
 
                     <div className='instructions'>
                         <p className='where-p'>{get('left text')}</p>
@@ -162,23 +164,11 @@ function English() {
 
             </div>
         </div>
-
-            {error && <p>{error}</p>}
-
-            <div className='oh-container'>
-                <h2 className='oh-h2'>Opening Hours</h2>
-                <p className='where-p'>Monday {get('oh Monday')}</p>
-                <p className='where-p'>Tuesday {get('oh Tuesday')}</p>
-                <p className='where-p'>Wednesday {get('oh Wednesday')}</p>
-                <p className='where-p'>Thursday {get('oh Thursday')}</p>
-                <p className='where-p'>Friday {get('oh Friday')}</p>
-            </div>
-
-
+        </div>
         <Footer/>
 
         </div>
     );
             }
 
-    export default English;
+    export default Frontpage;
